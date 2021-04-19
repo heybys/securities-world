c1bf3e2336b7052ddac40061d0ef55e3dcf66e39
heybys <byn0174@gmail.com>
2021-04-19 21:39:41 +0900

    modify query to get transaction years

--- src/main/java/me/heybys/securitiesworld/repository/TransactionLogRepository.java
+++ src/main/java/me/heybys/securitiesworld/repository/TransactionLogRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 
 public interface TransactionLogRepository extends JpaRepository<TransactionLog, Long> {
 
-    @Query(value = "SELECT DISTINCT SUBSTRING(date, 1, 4) AS year FROM transaction_log", nativeQuery = true)
+    @Query(value = "SELECT DISTINCT SUBSTRING(date, 1, 4) AS year FROM transaction_log WHERE cancel_yn = 'N'", nativeQuery = true)
     List<String> findTransactionLogYears();
 }
